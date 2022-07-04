6d5ce2cd441cae9266057ba66f08519c4696422b
raphactn <raphael.d.caetano48@gmail.com>
2022-07-04 00:35:20 -0300

    add build settings

---
+++ dist/next.config.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/** @type {import('next').NextConfig} */
+var nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true
+  }
+};
+module.exports = nextConfig;
